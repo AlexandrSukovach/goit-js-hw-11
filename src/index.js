39e98a3f378799494b5388e720c08435babcea08
Aleksandr Sukovach <alexsukovach@gmail.com>
2022-01-03 13:08:33 +0200

    task-11 refact files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,78 @@
+
+import hitsTpl from './templates/articles.hbs';
 import './css/styles.css';
+import NewsApiService from './js/news-servis';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import LoadMoreBtn from './js/components/load-more-btn';
+
+// ===api key for pixabay==25024610-b715c2d32e80bfb8f3c0998cb===
+
+const refs = {
+   searchForm: document.querySelector('.search-form'),
+   hitsContainer: document.querySelector('.gallery'),
+   loadMoreBtn: document.querySelector('.load-more'),
+   searchFormButton: document.querySelector('.search-form button'),
+   searchFormInput: document.querySelector('.search-form input'),
+   loadMoreBtnSpan: document.querySelector('.load-more span'),
+};
+
+//===============style and class=========================
+refs.searchFormButton.insertAdjacentHTML("afterbegin", "<span>&#128269;</span>");
+refs.loadMoreBtn.insertAdjacentHTML("afterbegin", "<span class='label'>Load more</span>");
+refs.loadMoreBtn.setAttribute('class', 'button load-more');
+refs.loadMoreBtn.setAttribute('data-action', 'load-more');
+refs.searchForm.setAttribute("style", "display: flex; justify-content: center; padding: 8px; margin-bottom: 24px; background-color: mediumblue;");
+refs.searchFormInput.setAttribute("style", "cursor: pointer;");
+refs.searchFormButton.setAttribute("style", "cursor: pointer;");
+refs.hitsContainer.setAttribute("style", "display: grid; grid-template-columns: repeat(auto-fill, minmax(300px, 1fr)); grid-gap: 20px; padding: 0; margin: 0; list-style: none;  max-width: calc(100vw - 80px); margin-left: auto; margin-right: auto; margin-bottom: 30px;");
+refs.loadMoreBtn.setAttribute("style", "margin: 0 auto; cursor: pointer; padding: 10px 20px; color: #fafafa; background-color: rgb(76, 76, 238); border-radius: 3px;");
+
+const pageLength = 40;
+const newsApiService = new NewsApiService();
+const loadMoreBtn = new LoadMoreBtn({
+   selector: '[data-action="load-more"]',
+   hidden: true,
+});
+
+refs.searchForm.addEventListener('submit', onSearch);
+loadMoreBtn.refs.button.addEventListener('click', fetchArticles);
+
+function onSearch(e) {
+   e.preventDefault();
+   newsApiService.query = e.currentTarget.elements.searchQuery.value;
+   if (newsApiService.query === '') {
+      return Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+   };
+   loadMoreBtn.show();
+   newsApiService.resetPage();
+   clearArticlesContainer();
+   fetchArticles();
+};
+
+function fetchArticles() {
+   loadMoreBtn.disable();
+   newsApiService.fetchArticles().then(hits => {
+      appendArticlesMarkup(hits);
+      loadMoreBtn.enable();
+   });
+};
+
+function appendArticlesMarkup(hits) {
+   if (hits.length === 0) {
+      refs.loadMoreBtn.setAttribute('class', 'load-more btn btn-primary button is-hidden');
+      return Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+   } else if (hits.length < pageLength && hits.length > 0) {
+      refs.loadMoreBtn.setAttribute('class', 'load-more btn btn-primary button is-hidden');
+      refs.hitsContainer.insertAdjacentHTML('beforeend', hitsTpl(hits));
+      return Notify.info("We're sorry, but you've reached the end of search results.");
+   }
+   refs.hitsContainer.insertAdjacentHTML('beforeend', hitsTpl(hits));
+};
+
+function clearArticlesContainer() {
+   refs.hitsContainer.innerHTML = '';
+};
+
+
 
 
---
+++ src/js/news-servis.js
@@ -0,0 +1,142 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+const axios = require('axios');
+export default class NewsApiService {
+   constructor() {
+      this.searchQuery = '';
+      this.page = 1;
+      this.reg_page = 0;
+      this.pageLength = 40;
+   };
+   // ================async/await================
+   async fetchArticles() {
+      // =================axios===================
+      const response = await axios.get(`https://pixabay.com/api/?key=25024610-b715c2d32e80bfb8f3c0998cb&image_type=photo&orientation=horizontal&safesearch=true&q=${this.searchQuery}&per_page=${this.pageLength}&page=${this.page}`)
+      // console.log(response.data.totalHits)
+      if (this.page === 1 && response.data.totalHits !== 0) {
+         Notify.success(`Hooray! We found ${response.data.totalHits} images.`);
+      }
+      let hits = response.data.hits
+      this.incrementPage();
+      return await hits;
+   };
+   incrementPage() {
+      this.page += 1;
+   };
+   resetPage() {
+      this.page = 1;
+   };
+   get query() {
+      return this.searchQuery;
+   };
+   set query(newQuery) {
+      this.searchQuery = newQuery;
+   };
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ===========================без-async/await/axios===============================
+
+// export default class NewsApiService {
+//    constructor() {
+//       this.searchQuery = '';
+//       this.page = 1;
+//       this.reg_page = 0;
+//       this.hit_leng = 0;
+//    };
+//    fetchArticles() {
+//       // console.log('do-', this);
+//       return fetch(`https://pixabay.com/api/?key=25024610-b715c2d32e80bfb8f3c0998cb&image_type=photo&orientation=horizontal&safesearch=true&q=${this.searchQuery}&per_page=40&page=${this.page}`)
+//          .then(response => {
+//             // console.log(response.json)
+//             return response.json()
+//          })
+//          .then(({ hits }) => {
+//             // console.log('this.reg_page-', this.reg_page)
+//             // this.reg_page += 40;
+//             // this.hit_leng += hits.length;
+//             // // console.log('this.hit_leng-', this.hit_leng)
+//             // // console.log('hits.length-', hits.length)
+//             // // console.log('this.reg_page-', this.reg_page)
+//             this.incrementPage();
+//             return hits;
+//          });
+//    };
+//    incrementPage() {
+//       this.page += 1;
+//       // console.log('this.page-', this.page)
+//       // this.reg_page += 40;
+//       // console.log('this.reg_page-', this.reg_page)
+//       // // this.hit_leng += hits.length;
+//       // console.log('this.hit_leng-', this.hit_leng)
+//       // console.log('hits.length-', this.hits.length)
+//    };
+//    resetPage() {
+//       this.page = 1;
+//    };
+//    get query() {
+//       return this.searchQuery;
+//    };
+//    set query(newQuery) {
+//       this.searchQuery = newQuery;
+//    };
+// };
